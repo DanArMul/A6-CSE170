530415a79bdc9bf2fdd2e116f270211140adf3f8
Daniel Muljono <dmuljono@ucsd.edu>
2021-02-25 11:45:46 +0700

    routes update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/secondPage', function(req, res, next) {
+  res.render('secondPage', { title: 'Express' });
+});
+
 module.exports = router;
